243b34d1bdc6593f5f0bf9235decdd395b90186f
felixidan <felix.idan@gmail.com>
2016-02-11 20:21:16 +0200

    problem instace - start

---
+++ src/com/android/academy/ProblemInstance.java
@@ -0,0 +1,19 @@
+package com.android.academy;
+
+/**
+ * Created by idan on 11/02/16.
+ */
+public class ProblemInstance {
+
+    // Simulation Parameters
+    public int rows;
+    public int cols;
+    public int numberOfDrowns;
+    public int deadline;
+    public int droneMaxLoad;
+
+    
+
+
+
+}
